6f043b9036ae6e21fd126660460d75b25c1dff2d
Haoqun Jiang <haoqunjiang@gmail.com>
2021-08-23 19:54:24 +0800

    test: enable `shell` option, fixing pnpm ENOENT errors

--- test.js
+++ test.js
@@ -14,7 +14,8 @@ for (const projectName of fs.readdirSync(playgroundDir)) {
     console.log(`Running unit tests in ${projectName}`)
     const unitTestResult = spawnSync('pnpm', ['test:unit:ci'], {
       cwd: path.resolve(playgroundDir, projectName),
-      stdio: 'inherit'
+      stdio: 'inherit',
+      shell: true
     })
     if (unitTestResult.status !== 0) {
       throw new Error(`Unit tests failed in ${projectName}`)
@@ -23,7 +24,8 @@ for (const projectName of fs.readdirSync(playgroundDir)) {
     console.log(`Running e2e tests in ${projectName}`)
     const e2eTestResult = spawnSync('pnpm', ['test:e2e:ci'], {
       cwd: path.resolve(playgroundDir, projectName),
-      stdio: 'inherit'
+      stdio: 'inherit',
+      shell: true
     })
     if (e2eTestResult.status !== 0) {
       throw new Error(`E2E tests failed in ${projectName}`)
