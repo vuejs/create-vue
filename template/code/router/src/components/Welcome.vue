8ee5c388333ec0bcc539b29c74d27f5c37e041d1
Haoqun Jiang <haoqunjiang@gmail.com>
2021-09-07 16:04:24 +0800

    chore: mention official @vuejs twitter account in the community section

--- template/code/router/src/components/Welcome.vue
+++ template/code/router/src/components/Welcome.vue
@@ -66,8 +66,9 @@ import SupportIcon from './icons/IconSupport.vue'
     <a target="_blank" href="https://chat.vuejs.org">Vue Land</a>, our official Discord server, or
     <a target="_blank" href="https://stackoverflow.com/questions/tagged/vue.js">StackOverflow</a>.
     You should also subscribe to
-    <a target="_blank" href="https://news.vuejs.org">our mailing list</a> for latest news in the Vue
-    world.
+    <a target="_blank" href="https://news.vuejs.org">our mailing list</a> and follow the official
+    <a target="_blank" href="https://twitter.com/vuejs">@vuejs</a>
+    twitter account for latest news in the Vue world.
   </WelcomeItem>
 
   <WelcomeItem>
