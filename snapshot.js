5c6de33fbca76092281d7e52389e81f60c074c47
Haoqun Jiang <haoqunjiang@gmail.com>
2021-09-18 17:31:44 +0800

    test: deal with errors thrown from child processes

--- snapshot.js
+++ snapshot.js
@@ -6,9 +6,18 @@ const playgroundDir = new URL('./playground/', import.meta.url).pathname
 function createProjectWithFeatureFlags(flags) {
   const projectName = flags.join('-')
   console.log(`Creating project ${projectName}`)
-  spawnSync('node', [bin, projectName, ...flags.map((flag) => `--${flag}`), '--force'], {
-    cwd: playgroundDir
-  })
+  const { status } = spawnSync(
+    'node',
+    [bin, projectName, ...flags.map((flag) => `--${flag}`), '--force'],
+    {
+      cwd: playgroundDir,
+      stdio: ['pipe', 'pipe', 'inherit']
+    }
+  )
+
+  if (status !== 0) {
+    process.exit(status)
+  }
 }
 
 const featureFlags = ['typescript', 'jsx', 'router', 'vuex', 'with-tests']
