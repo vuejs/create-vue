00dd60f8a44fa7aaf419227c3f86737b5964be0e
pangzebang <pangzebang@meituan.com>
2021-10-25 17:23:39 +0800

    fix: vite config

--- template/base/vite.config.js
+++ template/base/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname
+      '@': path.resolve(__dirname,'src')
     }
   }
 })
