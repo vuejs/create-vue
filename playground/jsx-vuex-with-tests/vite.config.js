51f3a925aa294348ce1df4c2f17a87b7a49ef685
Haoqun Jiang <haoqunjiang@gmail.com>
2021-11-25 20:31:59 +0800

    chore: prepublish sync

--- playground/jsx-vuex-with-tests/vite.config.js
+++ playground/jsx-vuex-with-tests/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@/': fileURLToPath(new URL('./src/', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
