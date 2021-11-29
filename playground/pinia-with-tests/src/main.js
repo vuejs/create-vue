c0a75326edc4ee40acf71f6744840119a5831101
Haoqun Jiang <haoqunjiang@gmail.com>
2021-11-29 16:57:29 +0800

    chore: update playground

---
+++ playground/pinia-with-tests/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
--- playground/typescript-vuex-with-tests/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-
-const app = createApp(App)
-
-app.use(store)
-
-app.mount('#app')
