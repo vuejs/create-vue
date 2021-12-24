8f0273f85a1d6544f11ee04c4b694a8d686a8178
Haoqun Jiang <haoqunjiang@gmail.com>
2021-12-24 14:02:06 +0800

    chore: update playground

--- playground/jsx-pinia/src/components/TheWelcome.vue
+++ playground/jsx-pinia/src/components/TheWelcome.vue
@@ -47,7 +47,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Ecosystem</template>
 
     Get official tools and libraries for your project:
-    <a target="_blank" href="https://next.vuex.vuejs.org/">Vuex</a>,
+    <a target="_blank" href="https://pinia.vuejs.org/">Pinia</a>,
     <a target="_blank" href="https://next.router.vuejs.org/">Vue Router</a>,
     <a target="_blank" href="https://next.vue-test-utils.vuejs.org/">Vue Test Utils</a>, and
     <a target="_blank" href="https://github.com/vuejs/devtools">Vue Dev Tools</a>. If you need more
