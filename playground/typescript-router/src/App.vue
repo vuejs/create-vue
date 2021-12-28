195a35e9a4455475ac281065330438bb2c694723
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-12-28 17:27:18 +0800

    fix: more accurate types for URL and vue-router (#23)

--- playground/typescript-router/src/App.vue
+++ playground/typescript-router/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
@@ -10,13 +11,13 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
 
-  <router-view />
+  <RouterView />
 </template>
 
 <style>
