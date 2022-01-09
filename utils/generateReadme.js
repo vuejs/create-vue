a03e813165f38ac19cabcf8650ea948c20a6ab88
Haoqun Jiang <haoqunjiang@gmail.com>
2022-01-09 16:29:49 +0800

    docs: no need to disable typescript vue plugin manually
    It's a plugin for "TypeScript and JavaScript Language Features", so once
    the latter is disabled, it will no longer work.

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -10,13 +10,10 @@ const sfcTypeSupportDoc = [
   '',
   "If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471) that is more performant. You can enable it by the following steps:",
   '',
-  '1. Disable the TypeScipt Vue Plugin',
-  "    1) Run `Extensions: Show Enabled Extensions` from VScode's command palette",
-  '    2) Find `TypeScript Vue Plugin (Volar)`, right click and select `Disable (Workspace)`',
-  '2. Disable the built-in TypeScript Extension',
+  '1. Disable the built-in TypeScript Extension',
   "    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette",
   '    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`',
-  '3. Reload the VSCode window by running `Developer: Reload Window` from the command palette.',
+  '2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.',
   ''
 ].join('\n')
 
