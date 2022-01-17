bb14f5cd8fdd825bb82bd5e54bb5a3fe5b2d0526
Haoqun Jiang <haoqunjiang@gmail.com>
2022-01-17 16:11:08 +0800

    chore: update the link for take over mode
    The new link contains instructions with screenshots, which is more
    user-friendly.

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -8,7 +8,7 @@ const sfcTypeSupportDoc = [
   '',
   'TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.',
   '',
-  "If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471) that is more performant. You can enable it by the following steps:",
+  "If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:",
   '',
   '1. Disable the built-in TypeScript Extension',
   "    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette",
