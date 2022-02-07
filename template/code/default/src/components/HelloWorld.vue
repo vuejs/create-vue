51b21881436e8544abdc7871cb7218912eb1b735
Haoqun Jiang <haoqunjiang@gmail.com>
2022-02-07 15:55:26 +0800

    chore: update links as Vue 3 becomes the new default

--- template/code/default/src/components/HelloWorld.vue
+++ template/code/default/src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ defineProps({
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://v3.vuejs.org/">Vue 3</a>.
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
     </h3>
   </div>
 </template>
--- template/code/router/src/components/TheWelcome.vue
+++ template/code/router/src/components/TheWelcome.vue
@@ -15,7 +15,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Documentation</template>
 
     Vue’s
-    <a target="_blank" href="https://v3.vuejs.org/">official documentation</a>
+    <a target="_blank" href="https://vuejs.org/">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
 
@@ -48,8 +48,8 @@ import SupportIcon from './icons/IconSupport.vue'
 
     Get official tools and libraries for your project:
     <a target="_blank" href="https://pinia.vuejs.org/">Pinia</a>,
-    <a target="_blank" href="https://next.router.vuejs.org/">Vue Router</a>,
-    <a target="_blank" href="https://next.vue-test-utils.vuejs.org/">Vue Test Utils</a>, and
+    <a target="_blank" href="https://router.vuejs.org/">Vue Router</a>,
+    <a target="_blank" href="https://test-utils.vuejs.org/">Vue Test Utils</a>, and
     <a target="_blank" href="https://github.com/vuejs/devtools">Vue Dev Tools</a>. If you need more
     resources, we suggest paying
     <a target="_blank" href="https://github.com/vuejs/awesome-vue">Awesome Vue</a>
