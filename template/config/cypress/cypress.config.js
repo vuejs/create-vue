d6d39a7200d8828e3070ee0d427a9a686eac6b35
Haoqun Jiang <haoqunjiang@gmail.com>
2022-06-03 10:41:43 +0800

    fix: fix syntax errors

--- template/config/cypress/cypress.config.js
+++ template/config/cypress/cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
