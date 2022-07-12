d8c9c76417ef527d5c4caefa7d85a925832860f5
Haoqun Jiang <haoqunjiang@gmail.com>
2022-07-12 18:22:02 +0800

    refactor: no need to use `@` path alias when `.` is sufficient

--- template/code/router/src/App.vue
+++ template/code/router/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
