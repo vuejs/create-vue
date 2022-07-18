6305f87f3171205ffbebcb5dabeed332366e0301
Haoqun Jiang <haoqunjiang@gmail.com>
2022-07-18 15:31:40 +0800

    refactor: use 'node:' protocols for Node.js built-in module imports

--- template/base/vite.config.js
+++ template/base/vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
--- utils/directoryTraverse.ts
+++ utils/directoryTraverse.ts
@@ -1,5 +1,5 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 
 export function preOrderDirectoryTraverse(dir, dirCallback, fileCallback) {
   for (const filename of fs.readdirSync(dir)) {
