1ecb73295610fb6989798c9c3a9f76405a511c63
Haoqun Jiang <haoqunjiang@gmail.com>
2022-08-11 16:56:44 +0800

    chore: also skip `.git` in preOrderDirectoryTraverse
    for consistency

--- utils/directoryTraverse.ts
+++ utils/directoryTraverse.ts
@@ -3,6 +3,9 @@ import * as path from 'node:path'
 
 export function preOrderDirectoryTraverse(dir, dirCallback, fileCallback) {
   for (const filename of fs.readdirSync(dir)) {
+    if (filename === '.git') {
+      continue
+    }
     const fullpath = path.resolve(dir, filename)
     if (fs.lstatSync(fullpath).isDirectory()) {
       dirCallback(fullpath)
