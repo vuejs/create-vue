f4ac339764ebd06bd23705bbaabb14fa229c31a6
Haoqun Jiang <haoqunjiang@gmail.com>
2022-09-04 12:52:21 +0800

    refactor!: use `@vue/create-eslint-config` for ESLint config generation
    Airbnb & Standard options are not added yet.
    
    The difference with the previous implementation is that we now
    use `@vue/eslint-config-typescript` instead of
    `@vue/eslint-config-typescript/recommended`, because I'm not very
    satisfied with the current implementation of the latter config.
    There are some edge cases that may confuse users.
    
    I need to find time to refactor that package in the coming months.

--- utils/renderEslint.ts
+++ utils/renderEslint.ts
@@ -3,50 +3,21 @@ import * as path from 'node:path'
 
 import type { Linter } from 'eslint'
 
-import { devDependencies as allEslintDeps } from '../template/eslint/package.json' assert { type: 'json' }
-import deepMerge from './deepMerge'
+import createESLintConfig from '@vue/create-eslint-config'
+
+import { devDependencies as eslintDeps } from '../template/eslint/package.json' assert { type: 'json' }
 import sortDependencies from './sortDependencies'
+import deepMerge from './deepMerge'
 
-const dependencies = {}
-function addEslintDependency(name) {
-  dependencies[name] = allEslintDeps[name]
-}
-
-addEslintDependency('eslint')
-addEslintDependency('eslint-plugin-vue')
-
-interface ESLintConfig extends Linter.Config {
-  extends: string[]
-}
-const config: ESLintConfig = {
-  root: true,
-  extends: ['plugin:vue/vue3-essential'],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
-
-function configureEslint({ language, styleGuide, needsPrettier, needsCypress, needsCypressCT }) {
-  switch (`${styleGuide}-${language}`) {
-    case 'default-javascript':
-      config.extends.push('eslint:recommended')
-      break
-    case 'default-typescript':
-      addEslintDependency('@vue/eslint-config-typescript')
-      config.extends.push('eslint:recommended')
-      config.extends.push('@vue/eslint-config-typescript/recommended')
-      break
-    // TODO: airbnb and standard
-  }
-
-  if (needsPrettier) {
-    addEslintDependency('prettier')
-    addEslintDependency('@vue/eslint-config-prettier')
-    config.extends.push('@vue/eslint-config-prettier')
-  }
+export default function renderEslint(
+  rootDir,
+  { needsTypeScript, needsCypress, needsCypressCT, needsPrettier }
+) {
+  const additionalConfig: Linter.Config = {}
+  const additionalDependencies = {}
 
   if (needsCypress) {
-    const cypressOverrides = [
+    additionalConfig.overrides = [
       {
         files: needsCypressCT
           ? ['**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}']
@@ -55,54 +26,39 @@ function configureEslint({ language, styleGuide, needsPrettier, needsCypress, ne
       }
     ]
 
-    addEslintDependency('eslint-plugin-cypress')
-    config.overrides = cypressOverrides
+    additionalDependencies['eslint-plugin-cypress'] = eslintDeps['eslint-plugin-cypress']
   }
 
-  // generate the configuration file
-  let configuration = '/* eslint-env node */\n'
-  if (styleGuide !== 'default' || language !== 'javascript' || needsPrettier) {
-    addEslintDependency('@rushstack/eslint-patch')
-    configuration += `require("@rushstack/eslint-patch/modern-module-resolution");\n\n`
-  }
-  configuration += `module.exports = ${JSON.stringify(config, undefined, 2)}\n`
-
-  return {
-    dependencies,
-    configuration
-  }
-}
-
-export default function renderEslint(
-  rootDir,
-  { needsTypeScript, needsCypress, needsCypressCT, needsPrettier }
-) {
-  const { dependencies, configuration } = configureEslint({
-    language: needsTypeScript ? 'typescript' : 'javascript',
+  const { pkg, files } = createESLintConfig({
+    vueVersion: '3.x',
     // we currently don't support other style guides
     styleGuide: 'default',
+    hasTypeScript: needsTypeScript,
     needsPrettier,
-    needsCypress,
-    needsCypressCT
+
+    additionalConfig,
+    additionalDependencies
   })
 
   // update package.json
   const packageJsonPath = path.resolve(rootDir, 'package.json')
   const existingPkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-  const pkg = sortDependencies(
-    deepMerge(existingPkg, {
+  const updatedPkg = sortDependencies(
+    deepMerge(deepMerge(existingPkg, pkg), {
       scripts: {
         // Note that we reuse .gitignore here to avoid duplicating the configuration
         lint: needsTypeScript
           ? 'eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore'
           : 'eslint . --ext .vue,.js,.jsx,.cjs,.mjs --fix --ignore-path .gitignore'
-      },
-      devDependencies: dependencies
+      }
     })
   )
-  fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2) + '\n')
+  fs.writeFileSync(packageJsonPath, JSON.stringify(updatedPkg, null, 2) + '\n', 'utf-8')
 
-  // write to .eslintrc.cjs
-  const eslintrcPath = path.resolve(rootDir, '.eslintrc.cjs')
-  fs.writeFileSync(eslintrcPath, configuration)
+  // write to .eslintrc.cjs, .prettierrc.json, etc.
+  for (const [fileName, content] of Object.entries(files)) {
+    const fullPath = path.resolve(rootDir, fileName)
+    console.log(fullPath, content)
+    fs.writeFileSync(fullPath, content as string, 'utf-8')
+  }
 }
