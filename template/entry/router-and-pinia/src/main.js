e3d50c06dcd827170e1f53c6246ddddd7d545e27
Haoqun Jiang <haoqunjiang@gmail.com>
2023-04-28 15:56:24 +0800

    fix: put css import first in entry files
    So that styles in Vue components take precendence over it.
    
    Fixes #238
    Thanks @lovetingyuan for reporting.

--- template/entry/router-and-pinia/src/main.js
+++ template/entry/router-and-pinia/src/main.js
@@ -1,10 +1,10 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
 
 const app = createApp(App)
 
