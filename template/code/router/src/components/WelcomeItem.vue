815bbffb150640d3fbb87f493baeb2c1a5c48d3d
汪心禾 <wangxinhe06@gmail.com>
2023-05-13 21:28:19 +0800

    fix: relative position in h1 and WelcomeItem

--- template/code/router/src/components/WelcomeItem.vue
+++ template/code/router/src/components/WelcomeItem.vue
@@ -16,6 +16,7 @@
 .item {
   margin-top: 2rem;
   display: flex;
+  position: relative;
 }
 
 .details {
--- template/code/typescript-router/src/components/HelloWorld.vue
+++ template/code/typescript-router/src/components/HelloWorld.vue
@@ -19,6 +19,7 @@ defineProps<{
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
+  position: relative;
   top: -10px;
 }
 
