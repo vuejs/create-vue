78bc3d6a9d3b8121ff86041a30c5aa5202c9286f
Haoqun Jiang <haoqunjiang@gmail.com>
2023-07-23 17:29:33 +0800

    refactor: drop import assertions and inline prettierrcs
    The Import Assertions proposal is superceded by the Import Attributes
    proposal. <https://github.com/tc39/proposal-import-attributes>
    
    So Node.js support for this syntax might be deprecated or even dropped
    in the future.
    Inlining these JSON objects is safer, though a bit less maintainable.
    But I think it's acceptable for those rarely touched configs.

--- templates/prettierrcs.js
+++ templates/prettierrcs.js
@@ -1,11 +1,47 @@
-import defaultConfig from './prettierrc-default.json' assert { type: 'json' }
+const defaultConfig = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'none'
+}
 
-import airbnb from './prettierrc-airbnb.json' assert { type: 'json' }
-import standard from './prettierrc-standard.json' assert { type: 'json' }
+const airbnb = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false
+}
+
+const standard = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
 
 export {
   defaultConfig as default,
 
   airbnb,
-  standard,
+  standard
 }
