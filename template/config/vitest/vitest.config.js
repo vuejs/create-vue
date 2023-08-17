653f9531ae22a26b0c46ec24559129fa35249bf5
Haoqun Jiang <haoqunjiang@gmail.com>
2023-08-17 23:18:48 +0800

    fix: remove `transformMode` from vitest config
    It's no longer needed in vitest 0.34:
    
    > By default, tests with `jsdom` or `happy-dom` use `web` transform mode
    
    https://github.com/vitest-dev/vitest/releases/tag/v0.34.0

--- template/config/vitest/vitest.config.js
+++ template/config/vitest/vitest.config.js
@@ -9,10 +9,7 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      }
+      root: fileURLToPath(new URL('./', import.meta.url))
     }
   })
 )
