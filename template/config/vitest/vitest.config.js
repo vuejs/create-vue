10d84088282ed3bae203974ec3514da7f4dc1bce
Haoqun Jiang <haoqunjiang@gmail.com>
2023-08-18 11:34:12 +0800

    chore: import both `mergeConfig` and `defineConfig` from `vitest/config`
    With the typing fix in Vitest 0.34, it is again possible to use
    `defineConfig` and `mergeConfig` from the `vitest` package.

--- template/config/vitest/vitest.config.js
+++ template/config/vitest/vitest.config.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
