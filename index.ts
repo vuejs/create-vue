ac73b9a5442954a4c5ea62a3c2439e48854b51d8
luventa <luventa@outlook.com>
2023-09-27 23:17:41 +0800

    refactor: remove useless import statement

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import * as path from 'node:path'
 
 import minimist from 'minimist'
 import prompts from 'prompts'
-import { red, green, bold, yellow } from 'kolorist'
+import { red, green, bold } from 'kolorist'
 
 import ejs from 'ejs'
 
