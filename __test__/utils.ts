c340af23e437c7c689d940abb530b2519cf326a3
CoolPlayLin <CoolPlayLin@qq.com>
2023-11-18 15:13:42 +0800

    bugfix for key-checking

--- __test__/utils.ts
+++ __test__/utils.ts
@@ -1,13 +1,23 @@
+/**
+ *
+ * @param obj object that needs to be validated
+ * @param schema template for validation
+ * @returns whether missed some keys
+ */
 export function isValid(obj: Object, schema: Object) {
   for (let key in schema) {
     if (!obj.hasOwnProperty(key)) {
       return false
     }
-    if (typeof schema[key] === 'object' && schema[key] !== null) {
-      if (!isValid(obj[key], schema[key])) {
-        return false
-      } else if (typeof obj[key] !== schema[key]) {
-        return false
+    if (schema[key] !== null) {
+      if (typeof schema[key] === 'string') {
+        if (typeof obj[key] !== schema[key]) {
+          return false
+        }
+      } else if (typeof schema[key] === 'object') {
+        if (!isValid(obj[key], schema[key])) {
+          return false
+        }
       }
     }
   }
