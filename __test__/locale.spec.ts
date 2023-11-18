a2f2e908bc848f2f3526dd008b13a16908cbc36c
CoolPlayLin <CoolPlayLin@qq.com>
2023-11-18 22:06:20 +0800

    add locale validate

--- __test__/locale.spec.ts
+++ __test__/locale.spec.ts
@@ -4,22 +4,32 @@ import { readdirSync } from 'node:fs'
 import { Language } from '../utils/getLanguage'
 import { includeAllKeys, excludeKeys } from './utils'
 
-const locales = readdirSync(resolve(__dirname, '../locales'))
-
-describe('should include full keys', () => {
-    const structure: Language = require('../schema/locale.json')
-    locales.forEach((locale) => {
-        it(`for ${locale}`, () => {
-            expect(includeAllKeys(require(`../locales/${locale}`), structure)).toBeTruthy()
-        })
-    })
+const locales = readdirSync(resolve(__dirname, '../locales')).filter((file) => {
+  return file.includes('.json') // exclude unnecessary files
 })
 
-describe("shouldn't include unnecessary keys", () => {
-    const structure: Language = require('../schema/locale.json')
-    locales.forEach((locale) => {
-        it(`for ${locale}`, () => {
-            expect(excludeKeys(require(`../locales/${locale}`), structure)).toBeTruthy()
-        })
+describe('should match name regex', () => {
+  locales.forEach((locale) => {
+      it(`for ${locale}`, () => {
+        expect(locale).toMatch(/^[a-zA-Z]{2}(-[a-zA-Z]{2})*.json$/)
+      })
+  })
+})
+
+describe('should include full keys', () => {
+  const structure: Language = require('../schema/locale.json')
+  locales.forEach((locale) => {
+    it(`for ${locale}`, () => {
+      expect(includeAllKeys(require(`../locales/${locale}`), structure)).toBeTruthy()
     })
+  })
+})
+
+describe("should not include unnecessary keys", () => {
+  const structure: Language = require('../schema/locale.json')
+  locales.forEach((locale) => {
+    it(`for ${locale}`, () => {
+      expect(excludeKeys(require(`../locales/${locale}`), structure)).toBeTruthy()
+    })
+  })
 })
