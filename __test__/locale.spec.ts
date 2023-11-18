3444b9cc1934c6f58a5c92a67a574d4e86a356d5
CoolPlayLin <CoolPlayLin@qq.com>
2023-11-18 16:59:02 +0800

    🤔

--- __test__/locale.spec.ts
+++ __test__/locale.spec.ts
@@ -7,74 +7,7 @@ import { isValid } from './utils'
 const locales = readdirSync(resolve(__dirname, '../locales'))
 
 describe('should include full keys', () => {
-    const structure: Language = {
-        "projectName": {
-            "message": "string",
-        },
-        "shouldOverwrite": {
-            "dirForPrompts": {
-                "current": "string",
-                "target": "string"
-            },
-            "message": "string"
-        },
-        "packageName": {
-            "message": "string",
-            "invalidMessage": "string",
-        },
-        "needsTypeScript": {
-            "message": "string",
-        },
-        "needsJsx": {
-            "message": "string",
-        },
-        "needsRouter": {
-            "message": "string",
-        },
-        "needsPinia": {
-            "message": "string",
-        },
-        "needsVitest": {
-            "message": "string",
-        },
-        "needsE2eTesting": {
-            "message": "string",
-            "hint": "string",
-            "selectOptions": {
-                "negative": {
-                    "title": "string",
-                },
-                "cypress": {
-                    "title": "string",
-                    "desc": "string",
-                },
-                "nightwatch": {
-                    "title": "string",
-                    "desc": "string",
-                },
-                "playwright": {
-                    "title": "string",
-                }
-            }
-        },
-        "needsEslint": {
-            "message": "string",
-        },
-        "needsPrettier": {
-            "message": "string",
-        },
-        "errors": {
-            "operationCancelled": "string",
-        },
-        "defaultToggleOptions": {
-            "active": "string",
-            "inactive": "string",
-        },
-        "infos": {
-            "scaffolding": "string",
-            "done": "string",
-        }
-    }
+    const structure: Language = require('../schema/locale.json')
     locales.forEach((locale) => {
         it(`for ${locale}`, () => {
             expect(isValid(require(`../locales/${locale}`), structure)).toBeTruthy()
