6665f0eb691a040fd81dbdf96e3bf13dee5bf749
Haoqun Jiang <haoqunjiang@gmail.com>
2023-12-06 01:33:40 +0800

    fix: add cypress support folder to eslint overrides
    Fixes #195

--- utils/renderEslint.ts
+++ utils/renderEslint.ts
@@ -24,9 +24,10 @@ export default function renderEslint(
         files: needsCypressCT
           ? [
               '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-              'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+              'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+              'cypress/support/**/*.{js,ts,jsx,tsx}'
             ]
-          : ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+          : ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
         extends: ['plugin:cypress/recommended']
       }
     ]
