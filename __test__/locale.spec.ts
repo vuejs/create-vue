b828a3943911656988259683514e9b6da6082a4d
CoolPlayLin <CoolPlayLin@qq.com>
2023-12-09 22:20:35 +0800

    fix: add ini as dev

--- __test__/locale.spec.ts
+++ __test__/locale.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { resolve } from 'node:path'
+import { parse } from 'ini'
 import { readdirSync } from 'node:fs'
 import en from '../locales/en-US.json'
 
@@ -22,7 +23,7 @@ const defaultKeys = getKeys(en);
 describe("locale files should include all keys", () => {
   localesOtherThanEnglish.forEach((locale) => {
     it(`for ${locale}`, () => {
-      expect(getKeys(require(`../locales/${locale}`))).toEqual(defaultKeys)
+      expect(getKeys(parse(`../locales/${locale}`))).toEqual(defaultKeys)
     })
   })
 })
