c99e4eed136145047dbaa49058439e61ab109bc3
BaboonKing <changfan.xu.up@qq.com>
2023-12-28 01:06:33 +0800

    feat: update to Vue 2.7.16

--- template/config/vitest/src/components/__tests__/HelloWorld.spec.js
+++ template/config/vitest/src/components/__tests__/HelloWorld.spec.js
@@ -5,6 +5,8 @@ import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
+    // TODO: https://github.com/vuejs/vue-test-utils/issues/2087
+    // @ts-ignore
     const wrapper = mount(HelloWorld, { propsData: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
