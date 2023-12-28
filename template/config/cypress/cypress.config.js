5ea4fdb84b48f3883a361adb899638a43fd5504c
BaboonKing <changfan.xu.up@qq.com>
2023-12-28 19:23:17 +0800

    feat: update to  Vite v5

--- template/config/cypress/cypress.config.js
+++ template/config/cypress/cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
