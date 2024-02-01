b1bae5e6c74e1b1c448eb8491d291f85217bdf04
BaboonKing <changfan.xu.up@qq.com>
2024-02-01 15:04:28 +0800

    fix: patch for @vue/test-utils@1.x with Vue@2.7.x

--- template/config/typescript/env.d.ts
+++ template/config/typescript/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+import type { ComponentPublicInstanceConstructor } from 'vue/types/v3-component-public-instance'
+
+/**
+ * patch for @vue/test-utils@1.x with Vue@2.7.x
+ * @see https://github.com/vuejs/vue-test-utils/issues/2087#issuecomment-1919820059
+ */
+declare module '@vue/test-utils' {
+  export function mount<T extends ComponentPublicInstanceConstructor>(
+    originalComponent: T,
+    options?: ThisTypedMountOptions<Vue>
+  ): Wrapper<Vue>
+}
