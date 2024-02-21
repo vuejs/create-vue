25907ae9c28c19114014a8dc1dc1c0e75ed5145e
btea <2356281422@qq.com>
2024-02-21 17:37:10 +0800

    fix: use needsNightwatchCT

--- utils/generateReadme.ts
+++ utils/generateReadme.ts
@@ -123,7 +123,7 @@ ${commandFor('test:e2e', '--debug')}
     `
   }
 
-  if (needsNightwatch) {
+  if (needsNightwatchCT) {
     npmScriptsDescriptions += `
 ### Run Headed Component Tests with [Nightwatch Component Testing](https://nightwatchjs.org/guide/component-testing/introduction.html)
   
