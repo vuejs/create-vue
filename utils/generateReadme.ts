24f52126e71034d01a4ab8188c8472478fb56a0c
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-03-04 09:11:56 +0200

    refactor: add volar reference

--- utils/generateReadme.ts
+++ utils/generateReadme.ts
@@ -4,7 +4,7 @@ const sfcTypeSupportDoc = [
   '',
   '## Type Support for `.vue` Imports in TS',
   '',
-  'TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking.',
+  'TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.',
   ''
 ].join('\n')
 
