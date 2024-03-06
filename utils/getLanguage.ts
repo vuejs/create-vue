31dd2e69d47d8f94b34edb769da09325b11cccae
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-03-06 16:31:05 +0200

    feat: init needsVueDevtools translations

--- utils/getLanguage.ts
+++ utils/getLanguage.ts
@@ -30,6 +30,7 @@ interface Language {
   needsE2eTesting: LanguageItem
   needsEslint: LanguageItem
   needsPrettier: LanguageItem
+  needsVueDevtools: LanguageItem
   errors: {
     operationCancelled: string
   }
