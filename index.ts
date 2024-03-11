c2d52a829f54a59d9f65aac6a82d3c2336aee7cc
Haoqun Jiang <haoqunjiang@gmail.com>
2024-03-11 22:27:49 +0800

    fix: targetDir should be inferred from positionals instead of args
    Fixes #464

--- index.ts
+++ index.ts
@@ -100,7 +100,7 @@ async function init() {
     devtools: { type: 'boolean' }
   } as const
 
-  const { values: argv } = parseArgs({
+  const { values: argv, positionals } = parseArgs({
     args,
     options,
     strict: false
@@ -123,7 +123,7 @@ async function init() {
       (argv.devtools || argv['vue-devtools'])
     ) === 'boolean'
 
-  let targetDir = args[0]
+  let targetDir = positionals[0]
   const defaultProjectName = !targetDir ? 'vue-project' : targetDir
 
   const forceOverwrite = argv.force
