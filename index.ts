8a7777d81c3d9b307b8a64088b52068c4f9a6c73
Ccjr Yueys <ccjr@CcjrdeMacBook-Air.local>
2024-03-18 19:07:09 +0800

    fix: Command description for Bun

--- index.ts
+++ index.ts
@@ -563,9 +563,15 @@ async function init() {
   }
 
   // Instructions:
-  // Supported package managers: pnpm > yarn > npm
+  // Supported package managers: pnpm > yarn > bun > npm
   const userAgent = process.env.npm_config_user_agent ?? ''
-  const packageManager = /pnpm/.test(userAgent) ? 'pnpm' : /yarn/.test(userAgent) ? 'yarn' : 'npm'
+  const packageManager = /pnpm/.test(userAgent)
+    ? 'pnpm'
+    : /yarn/.test(userAgent)
+      ? 'yarn'
+      : /bun/.test(userAgent)
+        ? 'bun'
+        : 'npm'
 
   // README generation
   fs.writeFileSync(
