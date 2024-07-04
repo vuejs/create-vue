081ba3eb0a197e89dae9ad57d096a2d549d87495
Raku Zeta <zetaraku@gmail.com>
2024-07-04 19:44:02 +0800

    fix: use conditional baseURL

--- template/config/playwright/playwright.config.js
+++ template/config/playwright/playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: process.env.CI ? 'http://localhost:4173' : 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
