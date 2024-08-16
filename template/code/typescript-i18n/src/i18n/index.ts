ad66d81eefa4b557a2460c46c230fef99360b8fc
Hanse Kim <procrustes5@gmail.com>
2024-08-16 23:36:41 +0900

    chore: test

--- template/code/typescript-i18n/src/i18n/index.ts
+++ template/code/typescript-i18n/src/i18n/index.ts
@@ -3,12 +3,11 @@ import enUS from './locales/en-US.json'
 
 type MessageSchema = typeof enUS
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const i18n = createI18n<[MessageSchema], 'en-US'>({
+const i18n = createI18n<[MessageSchema], 'en-US'>({
   locale: 'en-US',
   messages: {
     'en-US': enUS
   }
 })
 
-export type I18nInstance = typeof i18n
+export default i18n
