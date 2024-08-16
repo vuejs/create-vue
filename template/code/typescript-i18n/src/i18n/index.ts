026cd9e9332dafd4536daa9b2f58374c92f80258
Hanse Kim <procrustes5@gmail.com>
2024-08-16 23:17:02 +0900

    chore: avoid lint error

--- template/code/typescript-i18n/src/i18n/index.ts
+++ template/code/typescript-i18n/src/i18n/index.ts
@@ -3,11 +3,11 @@ import enUS from './locales/en-US.json'
 
 type MessageSchema = typeof enUS
 
-const i18n = createI18n<[MessageSchema], 'en-US'>({
+export const i18n = createI18n<[MessageSchema], 'en-US'>({
   locale: 'en-US',
   messages: {
     'en-US': enUS
   }
 })
 
-export default i18n
+export type I18nInstance = typeof i18n
