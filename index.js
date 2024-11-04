02f16c3bb4cdf3e8dfd0a4fe0a98fb24328e4e06
Haoqun Jiang <haoqunjiang@gmail.com>
2024-11-04 22:33:32 +0800

    fix(oxlint): should emit errors instead of warnings for correctness rules

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ export default function createConfig({
         },
       ],
     })
-    pkg.scripts['lint:oxlint'] = 'oxlint . --fix'
+    pkg.scripts['lint:oxlint'] = 'oxlint . --fix -D correctness'
     pkg.scripts['lint:eslint'] = 'eslint . --fix'
     pkg.scripts.lint = 'run-s lint:*'
   } else {
