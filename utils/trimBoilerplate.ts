f5291af51174bb790f9f59abdbbe97b181fae9db
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2025-01-08 07:10:25 +0100

    fix: use ts files when necessary with --bare option (#653)

--- utils/trimBoilerplate.ts
+++ utils/trimBoilerplate.ts
@@ -6,8 +6,7 @@ function replaceContent(filepath: string, replacer: (content: string) => string)
   fs.writeFileSync(filepath, replacer(content))
 }
 
-export default function trimBoilerplate(rootDir: string, features: Record<string, boolean>) {
-  const isTs = features.needsTypeScript
+export function trimBoilerplate(rootDir: string) {
   const srcDir = path.resolve(rootDir, 'src')
 
   for (const filename of fs.readdirSync(srcDir)) {
@@ -19,18 +18,21 @@ export default function trimBoilerplate(rootDir: string, features: Record<string
     const fullpath = path.resolve(srcDir, filename)
     fs.rmSync(fullpath, { recursive: true })
   }
+}
 
+export function removeCSSImport(rootDir: string, needsTypeScript: boolean) {
   // Remove CSS import in the entry file
-  const entryPath = path.resolve(rootDir, isTs ? 'src/main.ts' : 'src/main.js')
+  const entryPath = path.resolve(rootDir, needsTypeScript ? 'src/main.ts' : 'src/main.js')
   replaceContent(entryPath, (content) => content.replace("import './assets/main.css'\n\n", ''))
+}
 
+export function emptyRouterConfig(rootDir: string, needsTypeScript: boolean) {
+  const srcDir = path.resolve(rootDir, 'src')
   // If `router` feature is selected, use an empty router configuration
-  if (features.needsRouter) {
-    const routerEntry = path.resolve(srcDir, isTs ? 'router/index.ts' : 'router/index.js')
-    replaceContent(routerEntry, (content) =>
-      content
-        .replace(`import HomeView from '../views/HomeView.vue'\n`, '')
-        .replace(/routes:\s*\[[\s\S]*?\],/, 'routes: [],'),
-    )
-  }
+  const routerEntry = path.resolve(srcDir, needsTypeScript ? 'router/index.ts' : 'router/index.js')
+  replaceContent(routerEntry, (content) =>
+    content
+      .replace(`import HomeView from '../views/HomeView.vue'\n`, '')
+      .replace(/routes:\s*\[[\s\S]*?\],/, 'routes: [],'),
+  )
 }
