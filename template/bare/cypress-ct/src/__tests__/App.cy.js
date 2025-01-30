a7357398e1af44f057537f968e745ddb0ddf3e8b
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2025-01-30 17:10:27 +0100

    fix: enable e2e tests for --bare (#674)

--- template/bare/cypress-ct/src/__tests__/App.cy.js
+++ template/bare/cypress-ct/src/__tests__/App.cy.js
@@ -3,6 +3,6 @@ import App from '../App.vue'
 describe('App', () => {
   it('mounts and renders properly', () => {
     cy.mount(App)
-    cy.get('h1').should('contain', 'Hello World')
+    cy.get('h1').should('contain', 'You did it!')
   })
 })
