b2a93862cfebd44259b90f43f5e714cb3bf595b4
tommyhu <129492208+hiTommyhu@users.noreply.github.com>
2025-02-28 22:50:12 +0800

    chore(postversion): use `const` instead of `let` (#700)

--- scripts/postversion.mjs
+++ scripts/postversion.mjs
@@ -6,7 +6,7 @@ $.verbose = true
 await $`pnpm build`
 await $`pnpm snapshot`
 
-let { version } = JSON.parse(await fs.readFile('./package.json'))
+const { version } = JSON.parse(await fs.readFile('./package.json'))
 
 const playgroundDir = path.resolve(__dirname, '../playground/')
 cd(playgroundDir)
