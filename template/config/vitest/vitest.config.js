a960dee1c22898607b0a662f510aa991914c8d96
Ding Yuanhao <dingyuanhao14@126.com>
2025-03-01 12:19:59 +0800

    fix: vitest merge config in form of callback

--- template/config/vitest/vitest.config.js
+++ template/config/vitest/vitest.config.js
@@ -2,8 +2,8 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
+export default defineConfig(configEnv => mergeConfig(
+  viteConfig(configEnv),
   defineConfig({
     test: {
       environment: 'jsdom',
@@ -11,4 +11,4 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
   }),
-)
+))
