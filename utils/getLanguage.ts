27d834a778459402ac7c27127e935ab63a94196e
btea <2356281422@qq.com>
2025-06-13 16:56:30 +0800

    feat: add `rolldown-vite` features (#760)

--- utils/getLanguage.ts
+++ utils/getLanguage.ts
@@ -37,7 +37,10 @@ interface Language {
       [key: string]: { title: string; desc?: string; hintOnComponentTesting?: string }
     }
   }
+  needsExperimental: LanguageItem
+  needsExperimentalFeatures: LanguageItem
   needsOxlint: LanguageItem
+  needsRolldownVite: LanguageItem
   errors: {
     operationCancelled: string
   }
