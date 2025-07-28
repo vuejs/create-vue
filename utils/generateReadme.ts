5c55b14051a76d015b9490adda7365df048ce9b9
Haoqun Jiang <haoqunjiang@gmail.com>
2025-07-29 01:14:22 +0800

    docs: add browser setup instructions in the generated README [skip ci]
    I find that there are still many users who are not aware of the [custom
    object formatter feature](https://x.com/youyuxi/status/1316043978597376000)
    so I added the instructions for more visibility.

--- utils/generateReadme.ts
+++ utils/generateReadme.ts
@@ -30,6 +30,15 @@ This template should help get you started developing with Vue 3 in Vite.
 ## Recommended IDE Setup
 
 [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Recommended Browser Setup
+
+- Chromium-based browsers (Chrome, Edge, Brave, etc.):
+  - [Vue.js devtools](https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd) 
+  - [Turn on Custom Object Formmater in Chrome DevTools](http://bit.ly/object-formatters)
+- Firefox:
+  - [Vue.js devtools](https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/)
+  - [Turn on Custom Object Formmater in Firefox DevTools](https://fxdx.dev/firefox-devtools-custom-object-formatters/)
 ${needsTypeScript ? sfcTypeSupportDoc : ''}
 ## Customize configuration
 
