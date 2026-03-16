ead3a5bbe7acd6486590253f2a8fdceb58b96f93
btea <2356281422@qq.com>
2026-03-16 16:21:26 +0800

    chore: update

--- template/bare/vitest/src/__tests__/App.spec.js
+++ template/bare/vitest/src/__tests__/App.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vite-plus/test'
+import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
--- template/config/vitest/vitest.config.js
+++ template/config/vitest/vitest.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vite-plus'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
